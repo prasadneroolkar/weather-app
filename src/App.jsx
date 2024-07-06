9fac57b8bdaf0e297d31638a67eb390e7c0d2ed6
prasad <prasadneroolkar90@gmail.com>
2024-07-06 21:04:29 +0530

    App file edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import "./App.css";
+import { IoIosSearch } from "react-icons/io";
 
 function App() {
-  return <></>;
+  const [query, setQuery] = useState("");
+
+  const handleClick = () => {
+    console.log("clicked");
+  };
+  return (
+    <>
+      <div className="search-box">
+        <input
+          type="text"
+          value={query}
+          placeholder="search"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <IoIosSearch onClick={handleClick} />
+      </div>
+    </>
+  );
 }
 
 export default App;
