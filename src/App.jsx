140ccfe867bbe1317e6424e376a1b3feb356ff60
prasad <prasadneroolkar90@gmail.com>
2024-07-07 13:16:12 +0530

    fetching api is done

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,65 @@ import { IoIosSearch } from "react-icons/io";
 
 function App() {
   const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const api = {
+    key: "fd99ed79aaca0f833876596b62ad8dd0",
+    basename: "https://api.openweathermap.org/data/2.5/",
+  };
 
   const handleClick = () => {
-    console.log("clicked");
+    fetch(`${api.basename}weather?q=${query}&units=metric&appid=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+        setQuery("");
+      });
+  };
+
+  const handlePress = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.basename}weather?q=${query}&units=metric&appid=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "february",
+      "march",
+      "april",
+      "may",
+      "june",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let month = months[d.getMonth()];
+    let date = d.getDate();
+    let year = d.getFullYear();
+    return `${day},${date},${month},${year}`;
   };
   return (
     <>
@@ -16,9 +72,28 @@ function App() {
           value={query}
           placeholder="search"
           onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={handlePress}
         />
         <IoIosSearch onClick={handleClick} />
       </div>
+      {typeof weather.main != "undefined" ? (
+        <div>
+          <div className="location-box">
+            <div>
+              {weather.name},{weather.sys.country}
+            </div>
+            <div>{dateBuilder(new Date())}</div>
+          </div>
+          <div className="temperature-box">
+            <p>{Math.round(weather.main.temp)}°C</p>
+          </div>
+          <div className="weather">
+            <p>{weather.weather[0].main}</p>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 }
